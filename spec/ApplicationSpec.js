b3a4390f6a75078e29969604ee9d4297034893b8
yedhukrishnan <yedhu.k@multunus.com>
2013-08-23 14:57:54 +0530

    version 1: unrestricted, update 1

--- spec/ApplicationSpec.js
+++ spec/ApplicationSpec.js
@@ -1,7 +1,7 @@
 describe("index.html", function() {
-            it("should have an element to hold image", function() {
-                   expect($(".ball-area")).toExist();
-               }); 
+             it("should have an element to hold image", function() {
+                    expect($(".ball-area")).toExist();
+                }); 
              it("should have an image of the ball", function() {
                     expect($("img")).toHaveAttr("src", "images/ball.png");
                 });
@@ -9,28 +9,30 @@ describe("index.html", function() {
          });
 
 describe("Application", function() {
-             beforeEach(function() {
-                            $(".ball-area").data("left", "40");
-                            $(".ball-area").data("top", "45");
-                        });
-             it("should move the ball left when left arrow key is pressed", function() {
-                    var keyPress = jQuery.Event("keydown", { keyCode: 37 });
-                    $(document).trigger(keyPress);
-                    expect($(".ball-area").data("left")).toEqual(39);
-                });
-             it("should move the ball up when up arrow key is pressed", function() {
-                    var keyPress = jQuery.Event("keydown", { keyCode: 38 });
-                    $(document).trigger(keyPress);
-                    expect($(".ball-area").data("top")).toEqual(44);
-                });
-             it("should move the ball right when right arrow key is pressed", function() {
-                    var keyPress = jQuery.Event("keydown", { keyCode: 39 });
-                    $(document).trigger(keyPress);
-                    expect($(".ball-area").data("left")).toEqual(41);
-                });
-             it("should move the ball down when down arrow key is pressed", function() {
-                    var keyPress = jQuery.Event("keydown", { keyCode: 40 });
-                    $(document).trigger(keyPress);
-                    expect($(".ball-area").data("top")).toEqual(46);
-                });
+             describe("Ball Movement", function() {
+                          beforeEach(function() {
+                                         $(".ball-area").data("left", "40");
+                                         $(".ball-area").data("top", "45");
+                                     });
+                          it("should move the ball left when left arrow key is pressed", function() {
+                                 var keyPress = jQuery.Event("keydown", { keyCode: 37 });
+                                 $(document).trigger(keyPress);
+                                 expect($(".ball-area").data("left")).toEqual(39);
+                             });
+                          it("should move the ball up when up arrow key is pressed", function() {
+                                 var keyPress = jQuery.Event("keydown", { keyCode: 38 });
+                                 $(document).trigger(keyPress);
+                                 expect($(".ball-area").data("top")).toEqual(44);
+                             });
+                          it("should move the ball right when right arrow key is pressed", function() {
+                                 var keyPress = jQuery.Event("keydown", { keyCode: 39 });
+                                 $(document).trigger(keyPress);
+                                 expect($(".ball-area").data("left")).toEqual(41);
+                             });
+                          it("should move the ball down when down arrow key is pressed", function() {
+                                 var keyPress = jQuery.Event("keydown", { keyCode: 40 });
+                                 $(document).trigger(keyPress);
+                                 expect($(".ball-area").data("top")).toEqual(46);
+                             });
+                      });
          });
